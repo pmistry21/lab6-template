82f60d790d8637588c1cebad7923429dbca65686
Prasad Mistry <pmistry21@myseneca.ca>
2024-11-17 22:18:33 -0500

    Completed lab6a

---
+++ student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Name: Prasad Mistry
+# Student Number: 111964193
+# Student Email: pmistry21@myseneca.ca
+# Course: OPS445
+# Section: ZAA
+# Semester: Fall 2024
+
+class Student:
+
+    # Define the name and student ID number (which is a string) when a student object is created, ex. student1 = Student('john', '025969102')
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+        self.courses = {}
+
+    # Display student name and number
+    def displayStudent(self):
+        print('Student Name: ' + self.name)
+        print('Student Number: ' + self.number)
+
+    # Add a new course and grade to students record
+    def addGrade(self, course, grade):
+        self.courses[course] = grade
+
+    # Calculate the grade point average of all courses and display it
+    def displayGPA(self):
+        gpa = 0.0
+        for course in self.courses.keys():
+            gpa = gpa + self.courses[course]
+        print('GPA of student ' + self.name + ' is ' + str(gpa / len(self.courses)))
